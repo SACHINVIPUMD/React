72ae12b6e5f05aaf5767e6e9dd1b644539266153
SACHINVIPUMD <727822tuit201@skct.edu.in>
2023-12-12 15:55:34 +0530

    props and states

--- myapp/src/Component/ClassComponent.js
+++ myapp/src/Component/ClassComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
+import img from '../assets/login.jpg';
 class ClassComponent extends React.Component
 {
     render(){
         return(
             <div>
                 <h1> Hello class Component</h1>
+                <img src={img}></img>
             </div>
         );
     }
